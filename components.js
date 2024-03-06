94c129b987711e156d89642cd3fc89403a234429
tonegi <90652377+tonegi@users.noreply.github.com>
2024-03-06 19:21:23 -0300

    Delete components.js

--- components.js
+++
@@ -1,8 +0,0 @@
-function createHeader() {
-    const header = document.createElement('header');
-    header.innerHTML = '<h1>Avaliação contínua: Aula 03</h1>';
-    return header;
-}
-
-const header = createHeader();
-document.getElementById('header-template').appendChild(header);
